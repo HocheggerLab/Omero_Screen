f56e759d636b1d2084d0c83ec94bb7b5a65c5ae6
HaoranYueRyan <92607722+HaoranYueRyan@users.noreply.github.com>
2022-12-13 16:04:10 +0000

    Add files via upload

--- omero_screen/main.py
+++ omero_screen/main.py
@@ -25,7 +25,8 @@ def main(excel_path=EXCEL_PATH, conn=None):
     df_final = pd.concat([df_final.iloc[:,- 7:], df_final.iloc[:,:-7]], axis=1)
     df_final.to_csv(exp_paths.final_data / f"{exp_paths.plate_name}_final_data.csv")
     df_quality_control.to_csv(exp_paths.quality_ctr / f"{exp_paths.plate_name}_quality_data.csv")
-    plot_scatter_Edu_G2(data_dir=exp_paths.final_data, path_export=exp_paths.path, conn=conn, )
+    save_folder(Path_data=str(exp_paths.path))
+    plot_scatter_Edu_G2(data_dir=exp_paths.final_data, path_export=exp_paths.path, conn=conn,)
     plot_distribution_H3_P(data_dir=exp_paths.final_data, path_export=exp_paths.path, conn=conn)
 
 
