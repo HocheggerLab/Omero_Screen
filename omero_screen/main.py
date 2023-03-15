f88817aea2f4f914cc9d1f02e434a3feee9eccea
HaoranYueRyan <92607722+HaoranYueRyan@users.noreply.github.com>
2023-03-15 11:56:39 +0000

    add the gallery

--- omero_screen/main.py
+++ omero_screen/main.py
@@ -19,12 +19,6 @@ def main(plate_id, conn=None):
     df_final = pd.DataFrame()
     df_quality_control = pd.DataFrame()
     for count, well in enumerate(list(meta_data.plate_obj.listChildren())):
-        # print(well)
-        if count+1 in [96,95,94,93,92,91,90,89,87,88,86,
-                       85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,
-                       # 58,57,56,55,54,53,52,51,50,49,48,47,46,45,43,42,41,40,39,38,37,36,35,34,32,31,30,29,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,
-                       ]:
-            continue
         ann = well.getAnnotation(Defaults.NS)
         try:
             cell_line = dict(ann.getValue())['cell_line']
