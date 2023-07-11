e2a39359b080da5bd7d408c668a335a55bf9f6b2
HaoranYueRyan <92607722+HaoranYueRyan@users.noreply.github.com>
2023-07-11 21:22:11 +0100

    refact the code

--- omero_gallery/galleries_plot.py
+++ omero_gallery/galleries_plot.py
@@ -1,6 +1,7 @@
 #%%
 import matplotlib.pyplot as plt
 import random
+import cv2
 import numpy as np
 import tensorflow as tf
 from matplotlib.backends.backend_pdf import PdfPages
@@ -24,13 +25,48 @@ def channel_img_list(cell_data_list: list[tf.Tensor], channel : str) -> list[np.
         data_list = [resize_tf(i[:, :, channel_idx]).numpy().astype('float32') for i in cell_data_list]
     return data_list
 
-def plot_gallery(image_list:list[np.ndarray],check_phase,channels_option,nrows,gallery_name,path):
+def plot_gallery(image_list:list[np.ndarray],check_phase,channels_option,nrows,):
     # convert the TensorFlow Tensor object to a NumPy array
     nor_list = channel_img_list(cell_data_list=image_list,channel=channels_option)
-    plot_digits(nor_list, images_per_row=nrows,phase=check_phase,plot_name=gallery_name,save_path=path)
+    images=plot_digits(nor_list, images_per_row=nrows,phase=check_phase)
+    return images
 
 
-def plot_digits(sample, images_per_row, phase, plot_name, save_path):
+def add_border(image, border_size_ratio, border_color):
+    # Calculate the border size in pixels
+    border_size = int(border_size_ratio * min(image.shape[0], image.shape[1]))
+
+    # Add a border of the specified size and color around the image.
+    return cv2.copyMakeBorder(
+        image,
+        top=border_size,
+        bottom=border_size,
+        left=border_size,
+        right=border_size,
+        borderType=cv2.BORDER_CONSTANT,
+        value=border_color
+    )
+
+
+def merge_images(images, images_per_row, border_size_ratio, border_color):
+    # Assumes images is a list of (height, width, channels) images
+    assert len(images) % images_per_row == 0
+    images_per_col = len(images) // images_per_row
+
+    # Add border to each image
+    images_with_borders = [add_border(img, border_size_ratio, border_color) for img in images]
+
+    # Reshape the images into a grid
+    image_grid = [np.concatenate(images_with_borders[i:i + images_per_row], axis=1)  # concatenate images horizontally
+                  for i in range(0, len(images_with_borders), images_per_row)]  # for every row of images
+
+    # Concatenate the rows vertically
+    merged_image = np.concatenate(image_grid, axis=0)  # concatenate rows vertically
+
+    return merged_image
+
+
+def plot_digits(sample, images_per_row, phase,):
     # Get the shape of the individual images
     img_shape = sample[0].shape
     height, width = img_shape[:2]
@@ -47,25 +83,11 @@ def plot_digits(sample, images_per_row, phase, plot_name, save_path):
 
     # Add empty images to fill the last row
     images.extend([np.zeros((height, width, img_shape[2]))] * n_empty)
+    images = merge_images(images, images_per_row, border_size_ratio=0.03, border_color=(255, 0, 255))
+    plt.imshow(images)
+    plt.show()
+    return images
 
-    # Create the final grid of images
-    n_cols = images_per_row
-    fig, axs = plt.subplots(n_rows, n_cols, figsize=(n_cols * 1.5, n_rows * 1.5))
-
-    # Iterate through the axes and add images
-    for idx, ax in enumerate(axs.flat):
-        ax.imshow(images[idx])
-        ax.set_xticks([])
-        ax.set_yticks([])
-        # if idx < n_images:
-        #     ax.axis('off')
-    # Adjust the spacing between subplots
-    plt.subplots_adjust(hspace=0.03, wspace=0.03)
-    # Save the final image to a PDF
-    with PdfPages(f"{save_path}/{plot_name}_{phase}_Gallery.pdf") as pdf:
-        plt.suptitle(f'{plot_name} {phase} Gallery',fontsize=20)
-        pdf.savefig(fig)
-        plt.close()
 
 
 if __name__=='__main__':
@@ -77,4 +99,4 @@ if __name__=='__main__':
     df_M_mislabled = df_M[df_M['cell_cycle'] != 'M']
     data_list = [resize_tf(i).numpy().astype('float32') for i in df_M_mislabled['cell_data'].tolist()]
     # %%
-    plot_digits(data_list[:400],20, phase="mislabled_M", plot_name='check', save_path='/Users/haoranyue/Desktop/OmeroScreen_test/')
+    plot_digits(data_list[:4],2)
--- omero_gallery/main_gallery.py
+++ omero_gallery/main_gallery.py
@@ -1,69 +1,129 @@
-import matplotlib.pyplot as plt
+import napari
+from qtpy.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel
 import pandas as pd
-from omero_gallery.galleries_plot import plot_gallery
 import os
-import random
-from omero_gallery.gen_functions_gallery import get_gallery_df,get_cell_phase_id,cell_data_extraction
+from omero_gallery.galleries_plot import plot_gallery
+import numpy as np
+import matplotlib.pyplot as plt
+from omero_gallery.gen_functions_gallery import get_cell_phase_id,cell_data_extraction
+
+def get_gallery_df(df,plate_id,well=None,cell_line=None,condition='siCtr'):
+    """
+    Return a filtered version of the input Dataframe based on the plate_id, welll, cell_line,condition
+    """
+    df_gallery = df.loc[df['plate_id'] == int(plate_id)]
+    if well is not None:
+       df_gallery = df_gallery[df_gallery['well_id'] == well]
+    if cell_line is not None:
+       df_gallery = df_gallery[df_gallery['cell_line'] == cell_line]
+    if condition is not None:
+       df_gallery = df_gallery[df_gallery['condition'] == condition]
+
+    return df_gallery
 
 
+def processing_image(plate_id, file_path, num_rows, num_cols,well,condition, cell_line,cell_phase,channel):
 
-def main():
-    # Ask user for path to csv file
-    file_path = input("Enter path to cellcycle_detailed file: ")
-    df=pd.read_csv(str(file_path))
-    save_dir=os.path.dirname(file_path)
-    # Ask user for number of rows and columns
-    num_rows = int(input("Enter the number of rows: "))
-    num_cols = int(input("Enter the number of columns: "))
-    # Ask user for plate_id
-    plate_id = int(input("Enter the plate id: "))
+    df = pd.read_csv(str(file_path))
+    # if well is not None:
+    #     df_gallery = get_gallery_df(df, plate_id, well=well, cell_line=cell_line, condition=condition)
+    #     if cell_line is not None:
+    #         df_gallery = get_gallery_df(df, plate_id, well=well, cell_line=cell_line, condition=condition)
+    #         if condition is not None:
+    #             df_gallery = get_gallery_df(df, plate_id, well=well, cell_line=cell_line, condition=condition)
+    # else:
+    #     # df_gallery=get_gallery_df(df,plate_id,well=well,cell_line=cell_line,condition=condition)
+    df_gallery=df
 
-    # Initialize variables for user input
-    well = cell_line = condition = image_ids = None
-    # Ask user if they want to select a specific well or all wells
-    if input("Do you want to select a specific well? (Yes/No): ").lower() == 'yes':
-        well = int(input('Enter the well_id: '))
-        if input("Do you want to select a cell_line? (Yes/No): ").lower()=='yes':
-            cell_line = input('Enter the cell_line: ')
-            if input("Do you want to select a condition? (Yes/No): ").lower() == 'yes':
-                condition = input('Enter the condition: ')
-                if input("Do you want to select specific image_id? (Yes/No): ").lower() == 'yes':
-                    image_id_input = input('Enter the image_Id (comma-separated for multiple ids): ')
-                    image_ids = [int(id.strip()) for id in image_id_input.split(',')]
-
-    df_gallery = get_gallery_df(df, plate_id, well=well, cell_line=cell_line, condition=condition, image_id=image_ids)
-    if df_gallery.empty:
-        print("No data found based on the provided filters. Please check your input parameters.")
-        return
-    # Ask user a specific cell cycle phase
-    phase_option = input("Please select adf specific cell cycle phase? (All/Sub-G1/Polyploid/G1/Early S/Late S/Polyploid(replicating)/G2/M) ")
-    # Ask user for a specific channel
-    channels_option = input('Please select the specific channel? (All/Tubulin/Dapi) ')
-    # Ask user for a specific channel
-    name_option = str(input('Please input the specific name for saving? (for example: Screen_test) '))
-
-    if phase_option.lower()=='all':
-        cc_phases=["Sub-G1",'Polyploid', 'G1', 'Early S', 'Late S', 'Polyploid(replicating)', 'G2', 'M']
-
-    elif phase_option in ["Sub-G1",'Polyploid', 'G1', 'Early S', 'Late S', 'Polyploid(replicating)', 'G2', 'M']:
-        cc_phases = [phase_option.capitalize()]
-
+    if cell_phase in ["Sub-G1",'Polyploid', 'G1', 'Early S', 'Late S', 'Polyploid(replicating)', 'G2', 'M']:
+        cc_phases = [cell_phase.capitalize()]
     else:
         raise ValueError('Invalid inuput. Please enter a correct cell phase')
-
     for cc_phase in cc_phases:
         sample_ids = get_cell_phase_id(df=df_gallery, cell_phase=cc_phase, selected_num=num_cols * num_rows)
         filtered_images = cell_data_extraction(plate_id, sample_ids)
 
+    if filtered_images:  # Add this line to check if the list is not empty
+        image_gallery=plot_gallery(filtered_images, check_phase=cc_phase, channels_option=channel,
+                     nrows=num_rows)
 
-        if filtered_images:  # Add this line to check if the list is not empty
-            plot_gallery(filtered_images, check_phase=cc_phase, channels_option=channels_option,
-                         gallery_name=name_option,
-                         nrows=num_rows,
-                         path=save_dir)
-        else:
-            print(f"No images found for phase {cc_phase}. Skipping this phase.")
+    else:
+        print(f"No images found for phase {cc_phase}. Skipping this phase.")
 
-if __name__=="__main__":
+    return image_gallery
+
+
+class MyWidget(QWidget):
+    def __init__(self, viewer):
+        super().__init__()
+
+        self.viewer = viewer
+        self.setLayout(QVBoxLayout())
+
+
+
+        self.plate_id_edit = QLineEdit()
+        self.file_path_edit = QLineEdit()
+        self.num_rows_edit = QLineEdit()
+        self.num_cols_edit = QLineEdit()
+        self.Well_ID_edit = QLineEdit()
+        self.condition_edit = QLineEdit()
+        self.cell_line_edit = QLineEdit()
+        self.channel_edit = QLineEdit()
+        self.cell_phase_edit = QLineEdit()
+
+        self.layout().addWidget(QLabel('Plate ID:'))
+        self.layout().addWidget(self.plate_id_edit)
+        self.layout().addWidget(QLabel('File path:'))
+        self.layout().addWidget(self.file_path_edit)
+        self.layout().addWidget(QLabel('Num rows:'))
+        self.layout().addWidget(self.num_rows_edit)
+        self.layout().addWidget(QLabel('Num cols:'))
+        self.layout().addWidget(self.num_cols_edit)
+        self.layout().addWidget(QLabel('Well_ID:'))
+        self.layout().addWidget(self.Well_ID_edit)
+        self.layout().addWidget(QLabel('Condition:'))
+        self.layout().addWidget(self.condition_edit)
+        self.layout().addWidget(QLabel('Cell line:'))
+        self.layout().addWidget(self.cell_line_edit)
+        self.layout().addWidget(QLabel('Channel:'))
+        self.layout().addWidget(self.channel_edit)
+        self.layout().addWidget(QLabel('Cell phase:'))
+        self.layout().addWidget(self.cell_phase_edit)
+
+        self.btn = QPushButton('Start', self)
+        self.btn.clicked.connect(self.load_images)
+        self.layout().addWidget(self.btn)
+
+    def load_images(self):
+        plate_id = self.plate_id_edit.text()
+        file_path = self.file_path_edit.text()
+        num_rows = int(self.num_rows_edit.text())
+        num_cols = int(self.num_cols_edit.text())
+        well_id = self.Well_ID_edit.text()
+        condition = self.condition_edit.text()
+        cell_line = self.cell_line_edit.text()
+        channel = self.channel_edit.text()
+        cell_phase = self.cell_phase_edit.text()
+
+        # For this version, we're just printing the values
+        print(f"Plate ID: {plate_id}")
+        print(f"File path: {file_path}")
+        print(f"Num rows: {num_rows}")
+        print(f"Num cols: {num_cols}")
+        print(f"Well ID: {well_id}")
+        print(f"condition: {condition}")
+        print(f"cell_line: {cell_line}")
+        print(f"channel: {channel}")
+        print(f"Cell phase: {cell_phase}")
+        # Here you should put your code to load and visualize images using the given parameters.
+        self.image_gallery=processing_image(plate_id, file_path, num_rows, num_cols,well_id,condition, cell_line,cell_phase,channel)
+        self.viewer.add_image(self.image_gallery)
+
+def main():
+    viewer = napari.Viewer()
+    viewer.window.add_dock_widget(MyWidget(viewer), area='right')
+    napari.run()
+
+if __name__ == '__main__':
     main()
-
